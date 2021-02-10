8b7263705b84c3990e2b4eb0699b614db49293d0
Eliot Budde <oebudde13@gmail.com>
2021-02-10 09:40:13 -0500

    basic navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+
+import { Home, Login, Register } from "./pages";
+import MenuBar from "./components/MenuBar";
 import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Welcome to TwitterClone</h1>
-		</div>
+		<Router>
+			<Container>
+				<MenuBar />
+				<Route exact path="/" component={Home} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/register" component={Register} />
+			</Container>
+		</Router>
 	);
 }
 
---
+++ client/src/pages/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Register = () => {
+	return (
+		<div>
+			<h1>Register</h1>
+			<h2>Built on MERN + GraphQL</h2>
+		</div>
+	);
+};
+
+export default Register;
